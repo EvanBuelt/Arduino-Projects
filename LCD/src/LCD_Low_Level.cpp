f5bfe8da64be50d1fb4a898fa92c86b4bb366855
EvanBuelt <evanpunked@gmail.com>
2016-07-24 18:17:36 -0400

    Custom LCD Library
    This is a low level driver for an LCD using a Hitachi HD44780 or
    compatible chipset.  This library is based on the Arduino's Liquid
    Crystal library, except this provides low level functionality instead of
    high level functionality.  In addition, this library allows you to read
    as well as write.
    
    This class also does not have hard coded waits, but rather checks the
    busy flag (which the Liquid Crystal library does not).  As a result,
    this library runs slightly faster as a result.

---
+++ LCD/src/LCD_Low_Level.cpp
@@ -0,0 +1,375 @@
+﻿#include "LCD_develop.h"
+#include "Arduino.h"
+
+LCD_Low_Level::LCD_Low_Level(uint8_t rs_pin, uint8_t rw_pin, uint8_t enable_pin, 
+                             uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) {
+
+  init(1, rs_pin, rw_pin, enable_pin, 0, 0, 0, 0, d4, d5, d6, d7);
+}
+
+LCD_Low_Level::LCD_Low_Level(uint8_t rs_pin, uint8_t rw_pin, uint8_t enable_pin, 
+                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+                             uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) {
+
+  init(0, rs_pin, rw_pin, enable_pin, d0, d1, d2, d3, d4, d5, d6, d7);
+}
+
+void LCD_Low_Level::init(uint8_t fourbitmode, uint8_t rs_pin, uint8_t rw_pin, uint8_t enable_pin, 
+                         uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, 
+                         uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) {
+
+  _rs_pin = rs_pin;
+  _rw_pin = rw_pin;
+  _enable_pin = enable_pin;
+
+  _data_pins[0] = d0;
+  _data_pins[1] = d1;
+  _data_pins[2] = d2;
+  _data_pins[3] = d3;
+  _data_pins[4] = d4;
+  _data_pins[5] = d5;
+  _data_pins[6] = d6;
+  _data_pins[7] = d7;
+
+  if (fourbitmode) {
+    _display_function = LCD_4BitMode | LCD_1Line | LCD_5x8Dots;
+  } else {
+    _display_function = LCD_8BitMode | LCD_1Line | LCD_5x8Dots;
+  }
+
+  begin(16, 1);
+}
+
+void LCD_Low_Level::begin(uint8_t columns, uint8_t rows, uint8_t dotsize) {
+  if (rows > 1) {
+    _display_function |= LCD_2Line;
+  }
+
+  if ((dotsize != LCD_5x8Dots) && (rows == 1)) {
+    _display_function |= LCD_5x10Dots;
+  }
+
+  pinMode(_rs_pin, OUTPUT);
+  pinMode(_rw_pin, OUTPUT);
+  pinMode(_enable_pin, OUTPUT);
+
+  // According to datasheet, we need at least 40ms after power rises above 2.7V
+  // before sending commands.  Wait 50ms to be safe.
+  delayMicroseconds(50000);
+
+  // Pull RS and RW low to begin sending commands
+  digitalWrite(_rs_pin, LOW);
+  digitalWrite(_rw_pin, LOW);
+
+  // Put the LCD into 4 bit or 8 bit mode
+  if (! (_display_function & LCD_8BitMode)) {
+
+    // LCD starts in 8-bit mode.  Try to set 4 bit mode
+    write4bits(0x03);
+    delayMicroseconds(4500); // wait min 4.1 ms
+
+    // Second try
+    write4bits(0x03);
+    delayMicroseconds(4500); // wait min 4.1 ms
+
+    // Third try
+    write4bits(0x03);
+    delayMicroseconds(150);
+
+    // Fourth try will set the LCD to 4-bit interface
+    write4bits(0x02);
+  } else {
+
+    // Send function set command sequence
+    command(LCD_FunctionSet | _display_function);
+    delayMicroseconds(4500); // wait more than 4.1 ms
+
+    // Second try
+    command(LCD_FunctionSet | _display_function);
+    delayMicroseconds(150);
+
+    // Third try
+    command(LCD_FunctionSet | _display_function);
+  }
+
+  // Set # lines, font size, etc
+  command(LCD_FunctionSet | _display_function);
+
+  // turn the display on with no cursor or blinking default
+  displayControl(LCD_DisplayOn, LCD_CursorOff, LCD_BlinkOn);
+
+  // Clear the display
+  clearDisplay();
+
+  // Initialize default text direction for romance language
+  entrySetMode(LCD_CursorRight, LCD_DisplayLeft);
+  
+}
+
+// Direct instruction functions for LCD
+void LCD_Low_Level::clearDisplay() {
+  
+	waitUntilNotBusy();
+ 
+	command(LCD_ClearDisplay);
+	// delayMicroseconds(2000); // Must wait 1.53ms, so waiting 2ms to be safe.
+}
+
+void LCD_Low_Level::returnHome() {
+  
+	waitUntilNotBusy();
+ 
+	command(LCD_ReturnHome);
+	// delayMicroseconds(2000); // Must wait 1.53ms, so waiting 2ms to be safe.
+	
+}
+
+void LCD_Low_Level::entrySetMode(uint8_t direction, uint8_t shift_enable) {
+
+  waitUntilNotBusy();
+  
+	uint8_t data = LCD_EntryModeSet;
+	if (direction) {
+		data |= LCD_DisplayRight;
+	}
+	if (shift_enable) {
+		data |= LCD_CursorRight;
+	}
+	
+	command(data);
+	// delayMicroseconds(100); // Must wait 39us, so waiting 100us to be safe.
+}
+
+void LCD_Low_Level::displayControl(uint8_t display_on, uint8_t cursor_on, uint8_t blinking_on) {
+
+  waitUntilNotBusy();
+  
+	uint8_t data = LCD_DisplayControl;
+	if (display_on) {
+		data |= LCD_DisplayOn;
+	}
+	if (cursor_on) {
+		data |= LCD_CursorOn;
+	}
+	if (blinking_on) {
+		data |= LCD_BlinkOn;
+	}
+	command(data);
+	
+	// delayMicroseconds(100); // Must wait 39us, so waiting 100us to be safe
+}
+
+void LCD_Low_Level::cursorDisplayShift(uint8_t screen_shift, uint8_t direction) {
+  
+	waitUntilNotBusy();
+ 
+	uint8_t data = LCD_CursorShift;
+	if (screen_shift) {
+		data |= LCD_MoveDisplay;
+	}
+	if (direction) {
+		data |= LCD_MoveRight;
+	}
+	command(data);
+	
+	// delayMicroseconds(100); // Must wait 39us, so waiting 100us to be safe
+}
+
+void LCD_Low_Level::functionSet(uint8_t data_length, uint8_t number_lines, uint8_t display_font) {
+  
+	waitUntilNotBusy();
+ 
+	uint8_t data = LCD_FunctionSet;
+	if (data_length) {
+		data |= LCD_8BitMode;
+	}
+	if (number_lines) {
+		data |= LCD_2Line;
+	}
+	if (display_font) {
+		data |= LCD_5x10Dots;
+	}
+	command(data);
+	
+	// delayMicroseconds(100); // Must wait 39us, so waiting 100us to be safe
+}
+
+void LCD_Low_Level::setCGRAMAddress(uint8_t CGRAM_address) {
+
+  waitUntilNotBusy();
+  
+	uint8_t data = LCD_SetCGRAMAddress | (CGRAM_address & 0x3F);
+	command(data);
+	
+	// delayMicroseconds(100); // Must wait 39us, so waiting 100us to be safe
+}
+
+void LCD_Low_Level::setDDRAMAddress(uint8_t DDRAM_address) {
+
+  waitUntilNotBusy();
+  
+	uint8_t data = LCD_SetDDRAMAddress | (DDRAM_address & 0x7F);
+	command(data);
+	
+	// delayMicroseconds(100); // Must wait 39us, so waiting 100us to be safe
+}
+
+// Direct data functions for LCD
+uint8_t LCD_Low_Level::readAddress(void) {
+	
+	uint8_t data =  readData(0);
+	
+	data = (data & 0x7F);
+	return data;
+}
+
+uint8_t LCD_Low_Level::readBusyFlag(void) {
+	
+	return (readData(0) >> 7) & 0x01;
+}
+
+void LCD_Low_Level::writeDataToRAM(uint8_t data) {
+
+  waitUntilNotBusy();
+  
+	writeData(data);
+	
+	// delayMicroseconds(100); // Must wait 43us, so waiting 100us to be safe
+}
+
+uint8_t LCD_Low_Level::readDataFromRAM(void) {
+
+  waitUntilNotBusy();
+  
+	uint8_t data = 0;
+	data = readData(1);
+	
+	// delayMicroseconds(100); // Must wait 39us, so waiting 100us to be safe
+	
+	return data;
+}
+
+inline void LCD_Low_Level::writeControl(uint8_t rs_data, uint8_t rw_data) {
+  
+	digitalWrite(_rs_pin, rs_data);
+	digitalWrite(_rw_pin, rw_data);
+	delayMicroseconds(1);
+}
+
+void LCD_Low_Level::command(uint8_t data) {
+	
+	sendData(data, 0);
+}
+
+void LCD_Low_Level::writeData(uint8_t data) {
+	
+	sendData(data, 1);
+}
+
+uint8_t LCD_Low_Level::readData(uint8_t rs_bit) {
+	
+	return receive(rs_bit);
+}
+
+void LCD_Low_Level::sendData(uint8_t value, uint8_t rs_bit) {
+  
+	writeControl(rs_bit, 0);
+	
+	if (_display_function & LCD_8BitMode) {
+		write8bits(value);
+	} else {
+		write4bits((value & 0xF0)>>4);
+		write4bits(value & 0x0F);
+	}
+}
+
+uint8_t LCD_Low_Level::receive(uint8_t rs_bit) {
+	
+	writeControl(rs_bit, 1);
+	
+	int data = 0;
+	
+	if (_display_function & LCD_8BitMode) {
+		data = read8bits();
+	} else {
+		data = (read4bits() & 0x0F) << 4;
+		data = data | (read4bits() & 0x0F);
+	}
+	return data;
+}
+
+inline void LCD_Low_Level::waitUntilNotBusy() {
+  while(readBusyFlag()) {
+    delayMicroseconds(4);
+  }
+}
+
+inline uint8_t LCD_Low_Level::read4bits() {
+	uint8_t data = 0;
+
+  for (uint8_t i = 0; i < 4; i++) {
+    pinMode(_data_pins[4 + i], INPUT);
+  }
+  
+  pulseHigh();
+
+  for (uint8_t i = 0; i < 4; i++) {
+    data |= (digitalRead(_data_pins[4 + i]) & 0x01) << i;
+  }
+
+  pulseLow();
+  
+  return data;
+}
+
+inline uint8_t LCD_Low_Level::read8bits() {
+	int data = 0;
+
+	for (uint8_t i = 0; i < 8; i++) {
+    pinMode(_data_pins[i], INPUT);
+  }
+
+  pulseHigh();
+
+  for (uint8_t i = 0; i < 8; i++) {
+    data |= (digitalRead(_data_pins[i]) & 0x01) << i;
+  }
+
+  pulseLow();
+  
+	return data;
+}
+
+inline void LCD_Low_Level::write4bits(uint8_t data) {
+
+  for (uint8_t i = 0; i < 4; i++) {
+    pinMode(_data_pins[4 + i], OUTPUT);
+    digitalWrite(_data_pins[4 + i], (data >> i) & 0x01);
+  }
+  
+	pulseEnable();
+}
+inline void LCD_Low_Level::write8bits(uint8_t data) {
+
+  for (uint8_t i = 0; i < 8; i++) {
+    pinMode(_data_pins[i], OUTPUT);
+    digitalWrite(_data_pins[i], (data >> i) & 0x01);
+  }
+	
+	pulseEnable();
+}
+
+inline void LCD_Low_Level::pulseLow() {
+  digitalWrite(_enable_pin, LOW);
+  delayMicroseconds(1);
+}
+
+inline void LCD_Low_Level::pulseHigh() {
+  digitalWrite(_enable_pin, HIGH);
+  delayMicroseconds(1);
+}
+
+void LCD_Low_Level::pulseEnable() {
+	pulseHigh();
+	pulseLow();
+}
---
+++ LCD/src/LCD_Low_Level.h
@@ -0,0 +1,106 @@
+#ifndef LCD_Low_Level_h
+#define LCD_Low_Level_h
+
+#include <Arduino.h>
+
+// Commands
+#define LCD_ClearDisplay 0x01
+#define LCD_ReturnHome 0x02
+#define LCD_EntryModeSet 0x04
+#define LCD_DisplayControl 0x08
+#define LCD_CursorShift 0x10
+#define LCD_FunctionSet 0x20
+#define LCD_SetCGRAMAddress 0x40
+#define LCD_SetDDRAMAddress 0x80
+
+// Entry Mode Data
+#define LCD_CursorLeft 0x00
+#define LCD_CursorRight 0x01
+#define LCD_DisplayLeft 0x00
+#define LCD_DisplayRight 0x02
+
+// Display Control Data
+#define LCD_BlinkOff 0x00
+#define LCD_BlinkOn 0x01
+#define LCD_CursorOff 0x00
+#define LCD_CursorOn 0x02
+#define LCD_DisplayOff 0x00
+#define LCD_DisplayOn 0x04
+
+// Cursor Shift Data
+#define LCD_MoveLeft 0x00
+#define LCD_MoveRight 0x04
+#define LCD_MoveCursor 0x00
+#define LCD_MoveDisplay 0x08
+
+// Function Set Data
+#define LCD_5x8Dots 0x00
+#define LCD_5x10Dots 0x04
+#define LCD_1Line 0x00
+#define LCD_2Line 0x08
+#define LCD_4BitMode 0x00
+#define LCD_8BitMode 0x10
+
+class LCD_Low_Level {
+  public:
+ 
+    LCD_Low_Level(uint8_t, uint8_t, uint8_t,           // rs ,rw, and enable pins
+          	      uint8_t, uint8_t, uint8_t, uint8_t); // d4-d7 pins (4-bit mode)
+            
+    LCD_Low_Level(uint8_t, uint8_t, uint8_t,           // rs, rw, and enable pins
+                  uint8_t, uint8_t, uint8_t, uint8_t,  // d0-d3 pins
+                  uint8_t, uint8_t, uint8_t, uint8_t); // d4-d7 pins
+	
+    void init(uint8_t, uint8_t, uint8_t, uint8_t,  // 4-bit mode, rs, rw, and enable pins
+	          uint8_t, uint8_t, uint8_t, uint8_t,  // d0-d3
+	          uint8_t, uint8_t, uint8_t, uint8_t); // d4-d7
+
+    void begin(uint8_t columns, uint8_t rows, uint8_t charsize = LCD_5x8Dots);
+    // Direct instruction functions for LCD
+    void clearDisplay();
+    void returnHome();
+    void entrySetMode(uint8_t, uint8_t);
+    void displayControl(uint8_t, uint8_t, uint8_t);
+    void cursorDisplayShift(uint8_t, uint8_t);
+    void functionSet(uint8_t, uint8_t, uint8_t);
+    void setCGRAMAddress(uint8_t);
+    void setDDRAMAddress(uint8_t);
+	
+    // Direct data functions for LCD
+    uint8_t readAddress();
+    uint8_t readBusyFlag();
+    void writeDataToRAM(uint8_t);
+    uint8_t readDataFromRAM();
+
+  private: 
+  
+    // Eight commands, one write, and two read functions
+    void command(uint8_t);
+    void writeData(uint8_t);
+    uint8_t readData(uint8_t);
+	
+    // Support functions for writing to and reading from LCD
+    void writeControl(uint8_t, uint8_t);
+    void sendData(uint8_t, uint8_t);
+    uint8_t receive(uint8_t);
+    void waitUntilNotBusy();
+  
+    void write4bits(uint8_t);
+    void write8bits(uint8_t);
+	
+    uint8_t read4bits();
+    uint8_t read8bits(); 
+
+    void pulseLow();
+    void pulseHigh();
+    void pulseEnable();
+
+    uint8_t _data_pins[8];
+    uint8_t _rs_pin; // Low: Command.  High: Data
+    uint8_t _rw_pin; // Low: Write.  High: Read
+    uint8_t _enable_pin; // Pulse High for function
+
+    uint8_t _display_function;
+};
+
+#endif
